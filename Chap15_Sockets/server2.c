d6778181b652b3cce03165bcdde68f8117b7b580
sunwoo2 <abab702@naver.com>
2019-04-03 13:04:10 +0900

    -

---
+++ Chap15_Sockets/server2.c
@@ -0,0 +1,43 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+    int server_sockfd, client_sockfd;
+    int server_len, client_len;
+    struct sockaddr_in server_address;
+    struct sockaddr_in client_address;
+
+    // Remove any old sockets and create an unnamed socket for the server
+    unlink("server_socket");
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Name the socket;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_address.sin_port = 9734;
+    server_len = sizeof(server_address);
+    bind(server_sockfd, (struct sockaddr*)&server_address, server_len);
+
+    // Create a connection queue and wait for clients
+    listen(server_sockfd, 5);
+    while(1){
+        char ch;
+        printf("server waiting\n");
+
+        // Accept a connection
+        client_len = sizeof(client_address);
+        client_sockfd = accept(server_sockfd, (struct sockaddr*)&client_address, &client_len);
+
+        // Read and write to client on 'client_sockfd'
+        read(client_sockfd, &ch, 1);
+        ch++;
+        write(client_sockfd, &ch, 1);
+        close(client_sockfd);
+    }
+}
